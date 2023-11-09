54d45cc973705c2ec9b098d5269eaab2a9394c6c
Faz131 <fazms@icloud.com>
2023-11-08 19:56:05 -0700

    Added a fetch to movie API to test search.  Created a MovieListHeadings and SearchBox file

--- movie-picker/src/components/MovieList.js
+++ movie-picker/src/components/MovieList.js
@@ -7,10 +7,11 @@ const MovieList = (props) => {
 
     return (
         <>
-        <div>
+        
+        
             {props.movies.map((movie, index) => (
                 <div className='image-container d-flex justify-content-start m-3'>
-                    <img src={movie.Poster} alt='movie'></img>
+                    <img src={movie.Poster} alt='movie posters'></img>
                     <p>{movie.Title}</p>
                     <div
                         onClick={() => props.handleFavouritesClick(movie)}
@@ -20,7 +21,7 @@ const MovieList = (props) => {
                     </div>
                 </div>
             ))}
-        </div>
+        
         </>
     );
 };
